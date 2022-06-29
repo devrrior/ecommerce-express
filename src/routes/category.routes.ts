37f0dde77926672247b26f19e9fe6a37f9df3935
Fernando Guerrero <devrrior@gmail.com>
2022-06-29 13:25:26 -0500

    feat(routes): add put and delete route

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -4,6 +4,8 @@ import {
   createOneHandler,
   getByNameHandler,
   listHandler,
+  updateOneHandler,
+  deleteOneHandler,
 } from '../controllers/category.controller';
 
 const router = Router();
@@ -11,5 +13,7 @@ const router = Router();
 router.get('', listHandler);
 router.get('/:name', getByNameHandler);
 router.post('', createOneHandler);
+router.put('/:name', updateOneHandler);
+router.delete('/:name', deleteOneHandler);
 
 export default router;

0ce149eecdfb09ca7748b8d1449d96a22a785d56
Fernando Guerrero <devrrior@gmail.com>
2022-06-28 23:47:24 -0500

    feat(routes): add getById endpoint

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
 import {
-  createOneHandler,
   listHandler,
+  getByIdHandler,
+  createOneHandler,
 } from '../controllers/category.controller';
 
 const router = Router();
 
 router.get('', listHandler);
+router.get('/:id', getByIdHandler);
 router.post('', createOneHandler);
 
 export default router;

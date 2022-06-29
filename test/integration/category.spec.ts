ee2d7bfe19facd5066e084dfa5aa970de3b96e89
Fernando Guerrero <devrrior@gmail.com>
2022-06-28 23:30:07 -0500

    test(category): update test

--- test/integration/category.spec.ts
+++ test/integration/category.spec.ts
@@ -5,23 +5,33 @@ import request from 'supertest';
 import app from '../../src/config/app';
 import CategoryService from '../../src/services/category.service';
 
+const categoryPayload = {
+  _id: mongoose.Types.ObjectId.toString(),
+  name: 'technology',
+};
+
 const categoriesPayload = [
   {
-    id: new mongoose.Types.ObjectId().toString(),
-    name: 'Health',
+    name: 'health',
   },
   {
-    id: new mongoose.Types.ObjectId().toString(),
-    name: 'Sport',
+    name: 'sport',
   },
 ];
 
-describe('GET /categories', () => {
+describe('/categories', () => {
   beforeAll(async () => {
     // connect to mongodb
     const mongoServer = await MongoMemoryServer.create();
 
     await mongoose.connect(mongoServer.getUri());
+  });
+
+  afterEach(async () => {
+    const collections = Object.keys(mongoose.connection.collections);
+    for (const collectionName of collections) {
+      await mongoose.connection.collections[collectionName].drop();
+    }
   });
 
   afterAll(async () => {
@@ -30,17 +40,38 @@ describe('GET /categories', () => {
   });
 
   // POST /categories
-  it('Get a Category collection', async () => {
-    const categories = CategoryService.createMany(categoriesPayload);
-    const { statusCode, body } = await request(app).get('/api/v1/categories');
+  it('Create a category', async () => {
+    const { statusCode, body } = await request(app)
+      .post('/api/v1/categories')
+      .send(categoryPayload);
 
-    expect(statusCode).toBe(200);
-    expect(body).toBe(categories);
+    expect(statusCode).toBe(201);
+    expect(body.name).toMatch(categoryPayload.name);
   });
 
   // GET /categories
+  it('Get a Category collection', async () => {
+    const responseCategoryService = await CategoryService.createMany(
+      categoriesPayload
+    );
+    const { statusCode, body } = await request(app).get('/api/v1/categories');
+
+    expect(statusCode).toBe(200);
+    expect(body[0].name).toStrictEqual(responseCategoryService[0].name);
+    expect(body[1].name).toStrictEqual(responseCategoryService[1].name);
+  });
 
   // GET /categories/{id}
+  it('Find category by id', async () => {
+    await CategoryService.createOne(categoryPayload);
+
+    const { statusCode, body } = await request(app).get(
+      `/api/v1/categories/${categoryPayload._id}`
+    );
+
+    expect(statusCode).toBe(200);
+    expect(body.name).toMatch(categoryPayload.name);
+  });
 
   // PUT /categories/{id}
 

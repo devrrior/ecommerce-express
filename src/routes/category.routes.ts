f9b22a968ea717e7dcb10b1a6a8ab8ad89852f92
Fernando Guerrero <devrrior@gmail.com>
2022-06-28 23:30:52 -0500

    feat(routes): add routes for category

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 
+import {
+  createOneHandler,
+  listHandler,
+} from '../controllers/category.controller';
+
 const router = Router();
 
-router.get('', (_, res) => res.status(200).send('category'));
+router.get('', listHandler);
+router.post('', createOneHandler);
 
 export default router;

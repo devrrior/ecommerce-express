15a63268d21d6006fb968df8b1b565c2b605358f
Fernando Guerrero <devrrior@gmail.com>
2022-06-29 23:19:52 -0500

    feat(config): implement morgan in app.ts

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 
+import morganMiddleware from '../middlewares/morgan.middleware';
 import routes from '../routes';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+// set middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morganMiddleware);
 
 app.use(routes);
 

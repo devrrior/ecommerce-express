7341dad2c94b7ea9529a09a16469d23d45d005c2
Fernando Guerrero <devrrior@gmail.com>
2022-06-30 21:47:16 -0500

    test(user): create test for getById and createUser

---
+++ test/integration/user.spec.ts
@@ -0,0 +1,56 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import supertest from 'supertest';
+
+import app from '../../src/config/app';
+import UserService from '../../src/services/user.service';
+
+const userPayload = {
+  _id: new mongoose.Types.ObjectId().toString(),
+  email: 'devrrior@gmail.com',
+  password: 'fernando123',
+  passwordConfirmation: 'fernando123',
+  firstName: 'Fernando',
+  lastName: 'Guerrero',
+};
+
+describe('/users', () => {
+  beforeAll(async () => {
+    // connect to mongodb
+    const mongoServer = await MongoMemoryServer.create();
+
+    await mongoose.connect(mongoServer.getUri());
+  });
+
+  afterEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+    for (const connection of collections) {
+      await connection.deleteMany({});
+    }
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+  });
+
+  test('Create a user', async () => {
+    const { statusCode, body } = await supertest(app)
+      .post('/api/v1/users')
+      .send(userPayload);
+
+    expect(statusCode).toBe(201);
+    expect(body.firstName).toStrictEqual(userPayload.firstName);
+  });
+
+
+  test('Get a user by id', async () => {
+    await UserService.createOne(userPayload);
+    const { statusCode, body } = await supertest(app).get(`/api/v1/users/${userPayload._id}`);
+
+    expect(statusCode).toBe(200);
+    expect(body.email).toStrictEqual(userPayload.email);
+  });
+
+
+});

68a61c4f2e479e38d9c9863a07e7ca63c43ab8a8
Fernando Guerrero <devrrior@gmail.com>
2022-07-02 01:07:44 -0500

    feat(models): add private fields array

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -2,6 +2,8 @@ import { getModelForClass, prop } from '@typegoose/typegoose';
 
 import ICategory from '../interfaces/category.interface';
 
+export const categoryPrivateFields = ['__v'];
+
 class Category implements ICategory {
   @prop({ required: true, lowercase: true, unique: true })
   name: string;

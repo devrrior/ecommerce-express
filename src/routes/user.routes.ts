d9cdac01a5715cf185039a6804a9b2b2ecbd01fc
Fernando Guerrero <devrrior@gmail.com>
2022-07-07 12:45:24 -0500

    feat(routes): add list user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import {
   createUserHandler,
+  getListUserHandler,
   getUserByIdHandler,
 } from '../controllers/user.controller';
 import validateResource from '../middlewares/validateResource.middleware';
@@ -10,6 +11,7 @@ import { CreateUserSchema } from '../schemas/user.schema';
 const router = Router();
 
 router.post('', validateResource(CreateUserSchema), createUserHandler);
+router.get('', getListUserHandler);
 router.get('/:id', getUserByIdHandler);
 
 export default router;

37bd9a10b2b4c0992e3f728b625559b195ac4850
Fernando Guerrero <devrrior@gmail.com>
2022-07-07 12:42:18 -0500

    style(project): apply eslint

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -8,7 +8,9 @@ import CategoryService from '../services/category.service';
 const getListHandler = async (_: Request, res: Response) => {
   const categories = await CategoryService.getList(10, 0);
 
-  const payload = categories.map(category => omit(category, categoryPrivateFields));
+  const payload = categories.map((category) =>
+    omit(category, categoryPrivateFields)
+  );
 
   res.status(200).send(payload);
 };

c6e2c75f2e0b0dc839ba37df7c4f070db29e62f7
Fernando Guerrero <devrrior@gmail.com>
2022-07-11 00:36:02 -0500

    test(integration): add test for auth

---
+++ test/integration/auth.spec.ts
@@ -0,0 +1,47 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import supertest from 'supertest';
+
+import app from '../../src/config/app';
+import UserService from '../../src/services/user.service';
+
+const userPayload = {
+  email: 'devrrior@gmail.com',
+  password: 'fernando123',
+  passwordConfirmation: 'fernando123',
+  firstName: 'Fernando',
+  lastName: 'Guerrero',
+};
+
+describe('/tokens', () => {
+  beforeAll(async () => {
+    // connect to mongodb
+    const mongoServer = await MongoMemoryServer.create();
+
+    await mongoose.connect(mongoServer.getUri());
+  });
+
+  afterEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+    for (const connection of collections) {
+      await connection.deleteMany({});
+    }
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+  });
+
+  test('Generate JWT', async () => {
+    await UserService.createOne(userPayload);
+    const { statusCode } = await supertest(app)
+      .post('/api/v1/auth/token')
+      .send({
+        email: userPayload.email,
+        password: userPayload.password,
+      });
+
+    expect(statusCode).toBe(201);
+  });
+});

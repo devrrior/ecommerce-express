244a4cebca708d4aa6d9150b7ddc4a56640ff5dd
Fernando Guerrero <devrrior@gmail.com>
2022-07-22 04:19:19 -0500

    feat(jest.config): improve config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
-  },
-  moduleFileExtensions: ["ts", "js"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  testMatch: ["**/test/**/*.spec.(ts|js)"],
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.spec.ts'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
 };

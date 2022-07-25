58f93c1e197cbcb88a658a9e858ba524b2466a03
Fernando Guerrero <devrrior@gmail.com>
2022-07-25 00:54:25 -0500

    test(integration/category): improve test

--- test/integration/category.spec.ts
+++ test/integration/category.spec.ts
@@ -3,6 +3,7 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 
+import ITokens from '../../src/api/v1/interfaces/models/tokens.interface';
 import { Role } from '../../src/api/v1/interfaces/models/user.interface';
 import CategoryService from '../../src/api/v1/services/category.service';
 import UserService from '../../src/api/v1/services/user.service';
@@ -22,6 +23,10 @@ const categoryPayload = {
   name: 'technology',
 };
 
+const updateCategoryPayload = {
+  name: 'electronic',
+};
+
 const categoriesPayload = [
   {
     name: 'health',
@@ -33,19 +38,27 @@ const categoriesPayload = [
 
 const baseUrl = '/api/v1';
 
+let tokens: ITokens;
+
 describe('/categories', () => {
   beforeAll(async () => {
     // connect to mongodb
     const mongoServer = await MongoMemoryServer.create();
 
     await mongoose.connect(mongoServer.getUri());
-  });
 
-  afterEach(async () => {
-    const collections = await mongoose.connection.db.collections();
-    for (const connection of collections) {
-      await connection.deleteMany({});
-    }
+    await UserService.createOne({
+      ...userPayload,
+      password: await argon2.hash(userPayload.password),
+    });
+
+    await CategoryService.createMany(categoriesPayload);
+
+    const { body } = await request(app)
+      .post(`${baseUrl}/auth/token`)
+      .send({ email: userPayload.email, password: userPayload.password });
+
+    tokens = body;
   });
 
   afterAll(async () => {
@@ -55,20 +68,9 @@ describe('/categories', () => {
 
   // POST /categories
   it('Create a category', async () => {
-    await UserService.createOne({
-      ...userPayload,
-      password: await argon2.hash(userPayload.password),
-    });
-
-    const { statusCode: tokenStatusCode, body: tokenBody } = await request(app)
-      .post(`${baseUrl}/auth/token`)
-      .send({ email: userPayload.email, password: userPayload.password });
-
-    expect(tokenStatusCode).toBe(201);
-
     const { statusCode, body } = await request(app)
       .post(`${baseUrl}/categories`)
-      .set('Authorization', 'Bearer ' + tokenBody.access)
+      .set('Authorization', 'Bearer ' + tokens.access)
       .send(categoryPayload);
 
     expect(statusCode).toBe(201);
@@ -77,22 +79,17 @@ describe('/categories', () => {
 
   // GET /categories
   it('Get a Category collection', async () => {
-    const responseCategoryService = await CategoryService.createMany(
-      categoriesPayload
-    );
     const { statusCode, body } = await request(app).get(
       `${baseUrl}/categories`
     );
 
     expect(statusCode).toBe(200);
-    expect(body[0].name).toStrictEqual(responseCategoryService[0].name);
-    expect(body[1].name).toStrictEqual(responseCategoryService[1].name);
+    expect(body[0].name).toStrictEqual(categoriesPayload[0].name);
+    expect(body[1].name).toStrictEqual(categoriesPayload[1].name);
   });
 
   // GET /categories/{id}
   it('Find category by id', async () => {
-    await CategoryService.createOne(categoryPayload);
-
     const { statusCode, body } = await request(app).get(
       `${baseUrl}/categories/${categoryPayload.name}`
     );
@@ -103,25 +100,9 @@ describe('/categories', () => {
 
   // PUT /categories/{id}
   it('Update whole category', async () => {
-    await UserService.createOne({
-      ...userPayload,
-      password: await argon2.hash(userPayload.password),
-    });
-
-    const { statusCode: tokenStatusCode, body: tokenBody } = await request(app)
-      .post(`${baseUrl}/auth/token`)
-      .send({ email: userPayload.email, password: userPayload.password });
-
-    expect(tokenStatusCode).toBe(201);
-
-    await CategoryService.createOne(categoryPayload);
-    const updateCategoryPayload = {
-      name: 'electronic',
-    };
-
     const { statusCode, body } = await request(app)
       .put(`${baseUrl}/categories/${categoryPayload.name}`)
-      .set('Authorization', 'Bearer ' + tokenBody.access)
+      .set('Authorization', 'Bearer ' + tokens.access)
       .send(updateCategoryPayload);
 
     expect(statusCode).toBe(200);
@@ -130,21 +111,9 @@ describe('/categories', () => {
 
   // DELETE /categories/{id}
   it('Delete category', async () => {
-    await UserService.createOne({
-      ...userPayload,
-      password: await argon2.hash(userPayload.password),
-    });
-
-    const { statusCode: tokenStatusCode, body: tokenBody } = await request(app)
-      .post(`${baseUrl}/auth/token`)
-      .send({ email: userPayload.email, password: userPayload.password });
-
-    expect(tokenStatusCode).toBe(201);
-
-    await CategoryService.createOne(categoryPayload);
     const { statusCode } = await request(app)
-      .delete(`${baseUrl}/categories/${categoryPayload.name}`)
-      .set('Authorization', 'Bearer ' + tokenBody.access);
+      .delete(`${baseUrl}/categories/${updateCategoryPayload.name}`)
+      .set('Authorization', 'Bearer ' + tokens.access);
 
     expect(statusCode).toBe(204);
   });

9f46eca079d132bfb476a469b01231e026cbe41a
Fernando Guerrero <devrrior@gmail.com>
2022-08-04 14:07:03 -0500

    fix(config): add await in databaseConnect()

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,8 +3,8 @@ import swaggerDocs from '../api/v1/utils/swagger';
 import app from './app';
 import databaseConnect from './database';
 
-const server = app.listen(app.get('port'), () => {
-  databaseConnect();
+const server = app.listen(app.get('port'), async () => {
+  await databaseConnect();
 
   logger.info(
     `App is running at http://localhost:${app.get('port')} in ${app.get(

2078330528cd63c9aa21e1cfd0285af429221f11
Fernando Guerrero <devrrior@gmail.com>
2022-08-05 22:40:13 -0500

    test(integration/user): add tests for update and delete user

--- test/integration/user.spec.ts
+++ test/integration/user.spec.ts
@@ -1,3 +1,4 @@
+import argon2 from 'argon2';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
@@ -45,13 +46,108 @@ describe('/users', () => {
     expect(body.firstName).toStrictEqual(userPayloadWithoutID.firstName);
   });
 
+  it('Create user with an existing email', async () => {
+    const { statusCode } = await request(app)
+      .post('/api/v1/users')
+      .send(userPayloadWithoutID)
+      .set('Accept', 'application/json');
+
+    expect(statusCode).toBe(400);
+  });
+
   it('Get a user by id', async () => {
-    await UserService.createOne(userPayloadWithID);
+    await UserService.createOne({
+      ...userPayloadWithID,
+      password: await argon2.hash(userPayloadWithID.password),
+    });
     const { statusCode, body } = await request(app).get(
       `/api/v1/users/${userPayloadWithID._id}`
     );
 
     expect(statusCode).toBe(200);
     expect(body.email).toStrictEqual(userPayloadWithID.email);
+  });
+
+  it('Update User', async () => {
+    const newName = 'Luis';
+
+    const { statusCode: statusCodeTokens, body: bodyTokens } = await request(
+      app
+    )
+      .post('/api/v1/auth/tokens')
+      .send({
+        email: userPayloadWithID.email,
+        password: userPayloadWithID.password,
+      });
+
+    expect(statusCodeTokens).toBe(201);
+
+    const { statusCode, body } = await request(app)
+      .put(`/api/v1/users/${userPayloadWithID._id}`)
+      .set('Authorization', 'Bearer ' + bodyTokens.access)
+      .send({ firstName: newName });
+
+    expect(statusCode).toBe(200);
+
+    expect(body.firstName).toStrictEqual(newName);
+  });
+
+  it('Update User failed', async () => {
+    const newName = 'Luis';
+
+    const { statusCode: statusCodeTokens, body: bodyTokens } = await request(
+      app
+    )
+      .post('/api/v1/auth/tokens')
+      .send({
+        email: userPayloadWithoutID.email,
+        password: userPayloadWithoutID.password,
+      });
+    expect(statusCodeTokens).toBe(201);
+
+    const { statusCode } = await request(app)
+      .put(`/api/v1/users/${userPayloadWithID._id}`)
+      .set('Authorization', 'Bearer ' + bodyTokens.access)
+      .send({ firstName: newName });
+
+    expect(statusCode).toBe(403);
+  });
+
+  it('Delete User failed', async () => {
+    const { statusCode: statusCodeTokens, body: bodyTokens } = await request(
+      app
+    )
+      .post('/api/v1/auth/tokens')
+      .send({
+        email: userPayloadWithoutID.email,
+        password: userPayloadWithoutID.password,
+      });
+
+    expect(statusCodeTokens).toBe(201);
+
+    const { statusCode } = await request(app)
+      .delete(`/api/v1/users/${userPayloadWithID._id}`)
+      .set('Authorization', 'Bearer ' + bodyTokens.access);
+
+    expect(statusCode).toBe(403);
+  });
+
+  it('Delete User', async () => {
+    const { statusCode: statusCodeTokens, body: bodyTokens } = await request(
+      app
+    )
+      .post('/api/v1/auth/tokens')
+      .send({
+        email: userPayloadWithID.email,
+        password: userPayloadWithID.password,
+      });
+
+    expect(statusCodeTokens).toBe(201);
+
+    const { statusCode } = await request(app)
+      .delete(`/api/v1/users/${userPayloadWithID._id}`)
+      .set('Authorization', 'Bearer ' + bodyTokens.access);
+
+    expect(statusCode).toBe(204);
   });
 });

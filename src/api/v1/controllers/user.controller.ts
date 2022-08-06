4592fd87255554fd295c0639cd0a1ffcfe98b378
Fernando Guerrero <devrrior@gmail.com>
2022-08-05 22:42:25 -0500

    feat(api/v1/controllers/user.controller): add methods for update and delete user, add schemas as well

--- src/api/v1/controllers/user.controller.ts
+++ src/api/v1/controllers/user.controller.ts
@@ -4,15 +4,16 @@ import { omit } from 'lodash';
 
 import IUser from '../interfaces/models/user.interface';
 import { userPrivateFields } from '../models/user.model';
-import { CreateUserType } from '../schemas/user.schema';
+import {
+  CreateUserBodyType,
+  UpdateUserBodyType,
+  UpdateUserParamsType,
+  UserIdParamsType,
+} from '../schemas/user.schema';
 import UserService from '../services/user.service';
 
 const createUserHandler = async (
-  req: Request<
-    Record<string, unknown>,
-    Record<string, unknown>,
-    CreateUserType
-  >,
+  req: Request<unknown, unknown, CreateUserBodyType>,
   res: Response
 ) => {
   const { email, password, firstName, lastName } = req.body;
@@ -36,7 +37,8 @@ const createUserHandler = async (
     return res.status(201).send(payload);
   }
 
-  return res.status(400).send();
+  // return that message, because it's the unique error could be
+  return res.status(400).send({ message_error: 'Email is not available' });
 };
 
 const getListUserHandler = async (_: Request, res: Response) => {
@@ -50,10 +52,11 @@ const getListUserHandler = async (_: Request, res: Response) => {
   return res.status(404).send();
 };
 
-const getUserByIdHandler = async (req: Request, res: Response) => {
+const getUserByIdHandler = async (
+  req: Request<UserIdParamsType>,
+  res: Response
+) => {
   const { id } = req.params;
-
-  if (!id.match(/^[0-9a-fA-F]{24}$/)) return res.status(400).send();
 
   const user = await UserService.getById(id);
 
@@ -69,9 +72,50 @@ const getCurrentUserHandler = async (_: Request, res: Response) => {
   return res.status(200).send(omit(user, userPrivateFields));
 };
 
+const updateUserHandler = async (
+  req: Request<UpdateUserParamsType, unknown, UpdateUserBodyType>,
+  res: Response
+) => {
+  const { id } = req.params;
+  const userData = req.body;
+  const currentUser: IUser = res.locals.user;
+
+  const user = await UserService.patchById(id, userData);
+
+  if (!user) return res.status(404).send();
+
+  if (user.email !== currentUser.email) return res.status(403).send();
+
+  const payload = omit(user, userPrivateFields);
+
+  return res.status(200).send(payload);
+};
+
+const deleteUserHandler = async (
+  req: Request<UserIdParamsType>,
+  res: Response
+) => {
+  const { id } = req.params;
+  const currentUser: IUser = res.locals.user;
+
+  const user = await UserService.getById(id);
+
+  if (!user) return res.status(404).send();
+
+  if (user?.email !== currentUser.email) return res.status(403).send();
+
+  const checkIfUserWasDeleted = await UserService.deleteById(id);
+
+  if (!checkIfUserWasDeleted) return res.status(500).send();
+
+  return res.status(204).send();
+};
+
 export {
   createUserHandler,
+  deleteUserHandler,
   getCurrentUserHandler,
   getListUserHandler,
   getUserByIdHandler,
+  updateUserHandler,
 };

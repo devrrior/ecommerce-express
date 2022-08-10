02d26e15d64c364a3708b8437d5202706f095c6c
Fernando Guerrero <devrrior@gmail.com>
2022-08-09 23:15:05 -0500

    fix(src/api/v1/utils/mailer): fix import environment variables

--- src/api/v1/utils/mailer.ts
+++ src/api/v1/utils/mailer.ts
@@ -3,19 +3,19 @@ import nodemailer, { SendMailOptions } from 'nodemailer';
 import logger from './logger';
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: process.env.SMTP_SECURE === 'true',
   auth: {
-    user: process.env.SMPT_USER,
-    pass: process.env.SMPT_PASSWORD,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
   },
 });
 
 const sendEmail = async (payload: SendMailOptions) => {
   transporter.sendMail(payload, (error, info) => {
     if (error) {
-      logger.error(error);
+      logger.error(`${error} Error sending email`);
       return;
     }
     logger.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
